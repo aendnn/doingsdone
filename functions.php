25400581f222f040859d11d234852cf845a3decc
aendnn <aendnn9@gmail.com>
2018-11-23 22:31:19 +0200

    Добавляет функцию

--- functions.php
+++ functions.php
@@ -30,4 +30,16 @@ function count_tasks($tasks_list, $title_project) {
     }
     return $tasks_amount;
 };
+
+function date_count($end_date) {
+    $secs_in_hour = 3600;
+    $secs_in_minute = 60;
+    $cur_ts = time();
+    $end_ts = strtotime($end_date);
+    $ts_diff = $end_ts - $cur_ts;
+    $hours_until_end = floor($ts_diff / $secs_in_hour);
+    return $hours_until_end;
+};
+
+$date_show = print(date_count("next monday"));
 ?>
